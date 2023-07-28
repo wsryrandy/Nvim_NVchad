15b6dc90128f3704e68507c0d13dc975a531c2a6
CookG <randy131@gmail.com>
2023-07-28 11:41:43 -0400

    add debug trigger for setup c++ environment

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -15,6 +15,10 @@ M.dap = {
       "<cmd> DapToggleBreakpoint <CR>",
       "Add breakpoint at line"
     },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    },
     ["<leader>dus"] = {
       function ()
         local widgets = require('dap.ui.widgets');
